29eaa06251b3cae6afd426f78bd0cc84437f077e
Reinaldo León <reinaldoleon@gmail.com>
2021-07-10 09:40:34 +0200

    Test action bucket state (#1)
    * Test action bucket state
    
    * Test action bucket state change organization secret
    
    * Update terraform and aws provider version
    
    * Update action push branch pull request
    
    * Add template pull request
    
    Co-authored-by: Reinaldo León <reinaldo.leon@smartprotection.com>

---
+++ bucket-terraform-state/main.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "terraform-${var.project}-${var.env}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+  
+  tags = {
+    Name        = "terraform-${var.project}-${var.env}"
+    Environment = var.env
+    Creator     = var.creator
+    Terraform   = var.terraform
+  }
+}
+
+##CREATE DYNAMODB TABLE PARA BLOQUEAR EL ESTADO
+resource "aws_dynamodb_table" "terraform_state_locking" {
+  name = "terraform_state_locking-${var.project}-${var.env}"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags = {
+    Name        = "terraform-${var.project}-${var.env}"
+    Environment = var.env
+    Creator     = var.creator
+    Terraform   = var.terraform
+  }
+}
---
+++ bucket-terraform-state/provider.tf
@@ -0,0 +1,12 @@
+################################
+## CONFIGURATION AWS PRIVIDER ##
+################################
+terraform {
+  required_version = ">= 1.0.2"
+  required_providers {
+    aws = {
+      version = ">= 3.4"
+      source = "hashicorp/aws"
+    }
+  }
+}
